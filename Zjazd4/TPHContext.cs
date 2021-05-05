bec33e34b1afbb5a1c3c19feb92ad4cbbf08e3ac
nelumboo <tk.kielkowski@gmail.com>
2021-05-05 20:32:50 +0200

    Update solves problem with connectionString

--- Zjazd4/TPHContext.cs
+++ Zjazd4/TPHContext.cs
@@ -1,13 +1,20 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 
 namespace Zjazd4
 {
     public class TPHContext : DbContext
     {
+        static IConfigurationBuilder  builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: false);
+        static IConfiguration  configuration = builder.Build();
+            
+        string connectionString = configuration.GetConnectionString("myDb1");
+        
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(
-                "...");
+                connectionString);
             base.OnConfiguring(optionsBuilder);
         }
         
